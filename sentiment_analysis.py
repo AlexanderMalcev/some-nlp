bae0a289e4b73728789d20d07b14c472cff8052c
Alexnader <scaletta038@gmail.com>
2019-10-29 00:01:51 +0300

    refactored

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,37 +2,44 @@ from aiohttp import web
 from deeppavlov import build_model, configs
 
 
-model = build_model(configs.classifiers.rusentiment_bert)
+model = build_model(configs.classifiers.rusentiment_bert, download=False)
 
-app = web.Application(debug=True)
+app = web.Application(debug=False)
+
 sent = dict(
     negative=-1,
     positive=1,
     neutral=0,
-    skip=0,
-    speech=2,
+    skip=2,
+    speech=3,
 )
 
+
 async def sentiment_analysis(request: web.Request):
-    body: dict = await request.json()
-    text_id = body.get('id')
-    text = body.get('text')
-    try:
-        assert text_id is not None and text is not None
-    except AssertionError:
-        raise web.HTTPBadRequest(reason='Value mismatch')
-    result = model([text])
-    print(result)
-    try:
-        assert result[0] in sent 
-    except AssertionError:
-        raise web.HTTPExpectationFailed(reason=f'Unknown sentiment {result[0]}!')
-    return web.json_response(
-        {
-            'id':text_id,
-            'sentiment':sent.get(result[0])
-        }
-    )
+    body: list = await request.json()
+    result = []
+    while body:
+        data = {}
+        itm_body: dict = body.pop()
+        text = itm_body.get("text")
+        text_id = itm_body.get("id")
+
+        try:
+            assert text is not None and text_id is not None
+        except AssertionError:
+            raise web.HTTPBadRequest(reason="Value mismatch!")
+
+        sentiment = model([text])
+        
+        try:
+            assert sentiment[0] in sent
+        except AssertionError:
+            raise web.HTTPExpectationFailed(reason=f'Unknown sentiment {sentiment[0]}!')
+       
+        data["id"] = text_id
+        data["text"] = sent.get(sentiment[0])
+        result.append(data)
+    return web.json_response(sorted(result, key=lambda i:i["id"]))
 
 
 app.add_routes([
